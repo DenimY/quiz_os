0a9fb61a7d9a2ab0a2dfa4de392bd1080efa4c04
DESKTOP-432ANA8\ADMIN <ykm3976@gmail.com>
2020-09-22 17:29:05 +0900

    set

--- quiz/my-app/src/components/Question1.jsx
+++ quiz/my-app/src/components/Question1.jsx
@@ -1,8 +1,8 @@
 import React, {useState, useEffect} from "react";
 import styled from 'styled-components'
 
-import q1 from '../images/Q1/Group 1379.png'
-import q1_1 from '../images/Q1/Group 1379@2x.png'
+// import q1 from '../images/Q1/Group 1379.png'
+import q1 from '../images/Q1/Group 1379@2x.png'
 import point from '../images/common/Group 49.png'
 import quiz from '../images/Q1/Rectangle 23.png'
 
@@ -10,7 +10,8 @@ import btn1 from '../images/common/Group 1380@2x.png'
 
 
 const Question1Styled = styled.div`
-//height: 100%;
+height: 100%;
+width: 100%;
 
 //.Cont {
 //
@@ -22,31 +23,42 @@ const Question1 = () => {
 
     return (
         <Question1Styled>
-            <div className='Cont-main'>
-                <div className='cont-header'>
-                    <div className='header-title' style={{display: "flex"}}>
-                        <img src={q1}/>
+
+            <div className='cont-header'>
+                <div className='header-title' style={{display: "flex"}}>
+                    <img src={q1}/>
+                    <div>
+                        <br/>
+                        <br/>
                         <div>
-                            <br/>
-                            <br/>
-                            <div>-----------------------------------</div>
+                            ────────────────
+                            <line x1="0" y1="10" x2="30" y2="10" stroke="black" stroke-width="3"/>
+
                         </div>
                     </div>
                 </div>
+            </div>
 
-                <div className='title'>
-                    <div className='first'>
-                        첫번 째 장소,
-                        <img className='pointer_img' src={point}/>
-                    </div>
-                    <div className='second'>
-                        야생화공원
-                    </div>
+            <div className='title'>
+                <div className='first'>
+                    {/*<div>*/}
+                        첫 번째 장소,
+                    {/*</div>*/}
+                    <img className='pointer_img' src={point}/>
                 </div>
+
+                <div className='second'>
+                    야생화공원
+                </div>
+            </div>
+
+
+            <div className='Cont-main'>
+
 
                 {/*question image */}
                 <div className='cont-body'>
-                    야생화공원의 길을 걸으며 주변의 야생화를 둘러 보세요. 그리고 다음 식물의 이름을 차자아보세요.
+                    야생화공원의 길을 걸으며 주변의 야생화를 둘러 보세요. 그리고 다음 식물의 이름을 찾아보세요.
                     <img className='' src={quiz}>
                     </img>
                 </div>
